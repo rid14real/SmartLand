cc1577f81b4857fb1c3d9fc518aa1c071fbd69ee
JayJay <jadersonrilidio@gmail.com>
2021-04-15 16:33:21 -0300

    contract with all function implemented

--- backend-smart_contract/UserRegister.sol
+++ backend-smart_contract/UserRegister.sol
@@ -14,16 +14,17 @@ pragma solidity ^0.8.0;
 * The following variables/mappings/modifiers/functions could be implemented to the contract in order to achieve 
 * this security standard.
 */
+import "./node_modules/openzeppelin-solidity/contracts/utils/Counters.sol";
 
 contract UserRegister {
+    using Counters for Counters.Counter;
 
     // event emited when an user is added;
     // this event should be reviewed, maybe change the event name.
     event UserRegistered(address _userAddress, uint256 _registryNo, uint256 _id, string _name);
 
-    // generated registry number, for each user registered using registerUser function, it's incremented by 1
-    // should be initialized on constructor equals 0; 
-    uint256 registryCounter;
+    // auto-generate registry numbers for the users registered, by default, registryNumber initializes = 0.
+    Counters.Counter private registryNumber;
 
     // struct class to store user infos, note that the registryNo is automatically generated.
     struct UserInfo {
@@ -43,11 +44,8 @@ contract UserRegister {
     // return true if user has already registered on the system.
     mapping(address => bool) userExists;
 
-    // inside the constructor function as demonstration, the registryCounter should be initialyzed as 0:
-    constructor () {
-        registryCounter = 0;
-    }
-
+    // no need to initialize Counters.Counter on constructor anymore.
+    constructor () {}
     /*
      * @ for register new users on the system and allow then to hold/send/receive title NFTs as a security step;
      * @ the user address, ID and full name should be inserted on the function, the registryNumber
@@ -62,10 +60,11 @@ contract UserRegister {
             public returns (bool success) { //should add the keywork onlyOwner
         require(!(userExists[_address]), "User already registered"); // requires UserExists[_address] == false.
         userExists[_address] = true;
-        UserInfo memory infos = UserInfo(_address, registryCounter, _id, _name);
+        uint256 _registryNo = registryNumber.current();
+        UserInfo memory infos = UserInfo(_address, _registryNo, _id, _name);
         userInfo[_address] = infos;
-        emit UserRegistered(_address, registryCounter, _id, _name); // event should be reviewed
-        registryCounter++;
+        emit UserRegistered(_address, _registryNo, _id, _name); // event should be reviewed
+        registryNumber.increment();
         return true; 
     }
 
@@ -80,6 +79,32 @@ contract UserRegister {
         return (infos.userAddress, infos.registryNo, infos.id, infos.name);
     }
 
-    // more functions to be implemented?
-
+    /* 
+     * ADDITIONAL NOTES:
+     * the Function getUserInfo can be overloaded, in order to generate enhanced search parameters as follow below:
+     */
+    // mapping variables to track struct UserInfo:
+    mapping(uint256 => UserInfo) userInfoByRegistryNo;
+    mapping(uint256 => UserInfo) userInfoById;
+    mapping(string => UserInfo) userInfoByName;
+    // mapping variables to check is user exists is not mandatory, however:
+    mapping(uint256 => bool) userInfoByRegistryNoExists;
+    mapping(uint256 => bool) userInfoByIdExists;
+    mapping(string => bool) userInfoByNameExists;
+    //then implement the respective getter functions:
+    function getUserInfoByRegistryNo(uint256 _registryNo) public view returns (address, uint256, uint256, string memory) {
+        require(userInfoByRegistryNoExists[_registryNo], "User not registered");
+        UserInfo memory infos = userInfoByRegistryNo[_registryNo];
+        return (infos.userAddress, infos.registryNo, infos.id, infos.name);
+    }
+    function getUserInfoById(uint256 _Id) public view returns (address, uint256, uint256, string memory) {
+        require(userInfoByIdExists[_Id], "User not registered");
+        UserInfo memory infos = userInfoById[_Id];
+        return (infos.userAddress, infos.registryNo, infos.id, infos.name);
+    }
+    function getUserInfoByName(string memory _name) public view returns (address, uint256, uint256, string memory) {
+        require(userInfoByNameExists[_name], "User not registered");
+        UserInfo memory infos = userInfoByName[_name];
+        return (infos.userAddress, infos.registryNo, infos.id, infos.name);
+    }
 }
